dc68815cf2bceeab35af16b67d4e43f652829968
Dan Moseley <danomoseley@gmail.com>
2023-04-10 13:37:31 -0400

    Init react-app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import steamboat_ad from './steamboat_ad.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={steamboat_ad} className="steamboat-ad" alt="1884 Steamboat Ad" />
+    </div>
+  );
+}
+
+export default App;
