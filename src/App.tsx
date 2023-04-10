e51f2cdd48c734ab4f4930e5336e29b71c02eeaf
Dan Moseley <danomoseley@gmail.com>
2023-04-10 14:18:24 -0400

    Coming soon text

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="coming-soon">
+        <h1>Steamboat Maple syrup products will be available for purchase soon!</h1>
+        <p>
+          In the meantime, below is an 1883 ad for the steamboat route that included a stop at the location where our syrup is made.
+        </p>
+      </div>
       <img src={steamboat_ad} className="steamboat-ad" alt="1884 Steamboat Ad" />
     </div>
   );
